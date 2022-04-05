b4019ab05b2f270d14e8652eee42854d53dd294b
0xBA5E64 <26796481+0xBA5E64@users.noreply.github.com>
2022-04-05 19:01:32 +0100

    Appended lorem ipsum to word_bank

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let letter_elements = document.getElementById("words").getElementsByClassName("l
 // Word bank, list
 let word_bank = [];
     // Demo: Populate "word_bank" via sentence string
-    let example_phrase = "The quick brown fox jumps over the lazy dog";
+    let example_phrase = "The quick brown fox jumps over the lazy dog Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     word_bank = word_bank.concat(example_phrase.split(" "));
 
 // Carret
